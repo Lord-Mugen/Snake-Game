d528c96c059d612ebcbf852698af0c4555ac4098
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-26 12:02:02 -0500

    first commit

---
+++ snake.py
@@ -0,0 +1,10 @@
+from turtle import Screen, Turtle
+""" screen to create scenary """
+
+""" create"""
+screen= Screen() #intantiate Screen 
+screen.setup(width=600, height=600) #give size to the inside of the screen
+screen.bgcolor("black") #define the background color
+screen.title("Programate.snake") #title
+
+screen.exitonclick() 

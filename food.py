262bb2b59f8facd9cefb65eedc5caf96ced1f71a
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-28 14:17:59 -0500

    progress

---
+++ food.py
@@ -0,0 +1,11 @@
+from this import d
+from turtle import Turtle
+
+#generar numero aleatorio
+import random
+
+color = ["Blue", "Green"]
+class Food(Turtle):
+
+    def __init__(self):
+    
--- snake.py
+++ snake.py
@@ -1,10 +1,50 @@
-from turtle import Screen, Turtle
-""" screen to create scenary """
+from turtle import Turtle
 
-""" create"""
-screen= Screen() #intantiate Screen 
-screen.setup(width=600, height=600) #give size to the inside of the screen
-screen.bgcolor("black") #define the background color
-screen.title("Programate.snake") #title
+#creacion del cuerpo de la serpiente
+STARTING_POSITION = [(0,0), (-20, 0), (-40, 0)]
 
-screen.exitonclick() 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = [] #atributo
+        self.create_snake() #metodo
+        self.head = self-segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            snake_segment = Turtle("square")
+            snake_segment.color("white")
+            snake_segment.penup()
+            snake_segment.goto(position)
+            self.segments.append(snake_segment)
+
+    def move(self):
+        #movimiento de la serpiente
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.head.forward(20)
+        #head.left(90)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
